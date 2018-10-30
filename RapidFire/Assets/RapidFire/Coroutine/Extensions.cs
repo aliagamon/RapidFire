a6ab10263fea960e1a7418527cdf3dfdd0d3fa36
Ali Rezvani <3788964+aliagamon@users.noreply.github.com>
2018-10-31 01:00:59 +0330

    Added Wireframe and libraries needed.

---
+++ RapidFire/Assets/RapidFire/Coroutine/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RapidFire.Coroutine
+{
+    public static class Extentions
+    {
+        /// <summary>
+        /// Start a co-routine on a background thread.
+        /// </summary>
+        /// <param name="task">Gets a task object with more control on the background thread.</param>
+        /// <returns></returns>
+        public static UnityEngine.Coroutine StartCoroutineAsync(
+            this MonoBehaviour behaviour, IEnumerator routine, 
+            out Task task)
+        {
+            return Co.StartCoroutineAsync(behaviour, routine, out task);
+        }
+
+        /// <summary>
+        /// Start a co-routine on a background thread.
+        /// </summary>
+        public static UnityEngine.Coroutine StartCoroutineAsync(
+            this MonoBehaviour behaviour, IEnumerator routine)
+        {
+            Task t;
+            return StartCoroutineAsync(behaviour, routine, out t);
+        }
+    }
+}
